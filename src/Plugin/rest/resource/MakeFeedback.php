803642ebd987841443bf2ad797d532784acfebf6
kentkent <kent@solody.com>
2018-08-18 23:00:28 +0800

    fix bug

--- src/Plugin/rest/resource/MakeFeedback.php
+++ src/Plugin/rest/resource/MakeFeedback.php
@@ -23,131 +23,126 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
  *   }
  * )
  */
-class MakeFeedback extends ResourceBase
-{
+class MakeFeedback extends ResourceBase {
 
-    /**
-     * A current user instance.
-     *
-     * @var \Drupal\Core\Session\AccountProxyInterface
-     */
-    protected $currentUser;
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
 
-    /**
-     * Constructs a new MakeFeedback object.
-     *
-     * @param array $configuration
-     *   A configuration array containing information about the plugin instance.
-     * @param string $plugin_id
-     *   The plugin_id for the plugin instance.
-     * @param mixed $plugin_definition
-     *   The plugin implementation definition.
-     * @param array $serializer_formats
-     *   The available serialization formats.
-     * @param \Psr\Log\LoggerInterface $logger
-     *   A logger instance.
-     * @param \Drupal\Core\Session\AccountProxyInterface $current_user
-     *   A current user instance.
-     */
-    public function __construct(
-        array $configuration,
-        $plugin_id,
-        $plugin_definition,
-        array $serializer_formats,
-        LoggerInterface $logger,
-        AccountProxyInterface $current_user)
-    {
-        parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+  /**
+   * Constructs a new MakeFeedback object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
 
-        $this->currentUser = $current_user;
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('feedback'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Responds to POST requests.
+   *
+   * @param $data
+   * @return \Drupal\rest\ModifiedResourceResponse
+   *   The HTTP response object.
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function post($data) {
+
+    // You must to implement the logic of your REST Resource here.
+    // Use current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
-    {
-        return new static(
-            $configuration,
-            $plugin_id,
-            $plugin_definition,
-            $container->getParameter('serializer.formats'),
-            $container->get('logger.factory')->get('feedback'),
-            $container->get('current_user')
-        );
+    // 处理图片
+    $images = [];
+    if (isset($data['images']) && is_array($data['images'])) {
+      foreach ($data['images'] as $image_data) {
+        if ($image_data['base64']) {
+          $fileData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $image_data['base64']));
+          if ($fileData === FALSE) {
+            throw new BadRequestHttpException('Base64文件数据转换失败');
+          }
+
+          $directory = file_default_scheme() . '://feedback/feedback/images';
+          file_prepare_directory($directory, FILE_MODIFY_PERMISSIONS | FILE_CREATE_DIRECTORY);
+
+          // Determine image type
+          $f = finfo_open();
+          $mimeType = finfo_buffer($f, $fileData, FILEINFO_MIME_TYPE);
+          // Generate fileName
+          $ext = $this->getMimeTypeExtension($mimeType);
+
+          $file = file_save_data($fileData, file_default_scheme() . "://" . 'feedback/feedback/images/' . time() . \Drupal::service('uuid')->generate() . $ext, FILE_EXISTS_RENAME);
+          $images[] = $file;
+        }
+      }
     }
 
-    /**
-     * Responds to POST requests.
-     *
-     * @param $data
-     * @return \Drupal\rest\ModifiedResourceResponse
-     *   The HTTP response object.
-     * @throws \Drupal\Core\Entity\EntityStorageException
-     */
-    public function post($data)
-    {
+    $feedback = Feedback::create([
+      'user_id' => $this->currentUser->id(),
+      'title' => $this->currentUser->getDisplayName() . '的反馈',
+      'content' => $data['content'],
+      'images' => $images
+    ]);
 
-        // You must to implement the logic of your REST Resource here.
-        // Use current user after pass authentication to validate access.
-        if (!$this->currentUser->hasPermission('access content')) {
-            throw new AccessDeniedHttpException();
-        }
+    $feedback->save();
 
-        // 处理图片
-        $images = [];
-        if (isset($data['images']) && is_array($data['images'])) {
-            foreach ($data['images'] as $image_data) {
-                if ($image_data['base64']) {
-                    $fileData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $image_data['base64']));
-                    if ($fileData === FALSE) {
-                        throw new BadRequestHttpException('Base64文件数据转换失败');
-                    }
-                    $directory = \Drupal::service('file_system')->realpath(file_default_scheme() . "://");
-                    $directory .= '/feedback/feedback/images';
-                    file_prepare_directory($directory, FILE_MODIFY_PERMISSIONS | FILE_CREATE_DIRECTORY);
+    return new ModifiedResourceResponse($feedback, 200);
+  }
 
-                    // Determine image type
-                    $f = finfo_open();
-                    $mimeType = finfo_buffer($f, $fileData, FILEINFO_MIME_TYPE);
-                    // Generate fileName
-                    $ext = $this->getMimeTypeExtension($mimeType);
 
-                    $file = file_save_data($fileData, file_default_scheme() . "://" . 'feedback/feedback/images/' . time() . \Drupal::service('uuid')->generate() . $ext, FILE_EXISTS_RENAME);
-                    $images[] = $file;
-                }
-            }
-        }
-
-        $feedback = Feedback::create([
-            'user_id' => $this->currentUser->id(),
-            'title' => $this->currentUser->getDisplayName() . '的反馈',
-            'content' => $data['content'],
-            'images' => $images
-        ]);
-
-        $feedback->save();
-
-        return new ModifiedResourceResponse($feedback, 200);
+  protected function getMimeTypeExtension($mimeType) {
+    $mimeTypes = [
+      'image/png' => 'png',
+      'image/jpeg' => 'jpg',
+      'image/gif' => 'gif',
+      'image/bmp' => 'bmp',
+      'image/vnd.microsoft.icon' => 'ico',
+      'image/tiff' => 'tiff',
+      'image/svg+xml' => 'svg',
+    ];
+    if (isset($mimeTypes[$mimeType])) {
+      return '.' . $mimeTypes[$mimeType];
+    } else {
+      $split = explode('/', $mimeType);
+      return '.' . $split[1];
     }
-
-
-    protected function getMimeTypeExtension($mimeType) {
-        $mimeTypes = [
-            'image/png' => 'png',
-            'image/jpeg' => 'jpg',
-            'image/gif' => 'gif',
-            'image/bmp' => 'bmp',
-            'image/vnd.microsoft.icon' => 'ico',
-            'image/tiff' => 'tiff',
-            'image/svg+xml' => 'svg',
-        ];
-        if (isset($mimeTypes[$mimeType])) {
-            return '.' . $mimeTypes[$mimeType];
-        }
-        else {
-            $split = explode('/', $mimeType);
-            return '.' . $split[1];
-        }
-    }
+  }
 }
